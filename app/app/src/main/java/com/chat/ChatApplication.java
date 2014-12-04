b0cd4be8a184e35b52655d2933349a0d55b72fed
tommy <tommy.zhang@afusion.com>
2014-12-04 19:26:45 +0800

    add easemob chat into application

--- app/app/src/main/java/com/chat/ChatApplication.java
+++ app/app/src/main/java/com/chat/ChatApplication.java
@@ -1,8 +1,18 @@
 package com.chat;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.app.Instrumentation;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import com.easemob.chat.EMChat;
+import com.easemob.chat.EMChatManager;
+import com.easemob.chat.EMChatOptions;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by HQ_19 on 2014/12/2.
@@ -32,7 +42,33 @@ public class ChatApplication extends Application {
         instance = this;
         // Perform injection
         Injector.init(getRootModule(), this);
-
+        int pid = android.os.Process.myPid();
+        String processAppName = getAppName(pid);
+        // 如果使用到百度地图或者类似启动remote service的第三方库，这个if判断不能少
+        if (processAppName == null || processAppName.equals("")) {
+            // workaround for baidu location sdk
+            // 百度定位sdk，定位服务运行在一个单独的进程，每次定位服务启动的时候，都会调用application::onCreate
+            // 创建新的进程。
+            // 但环信的sdk只需要在主进程中初始化一次。 这个特殊处理是，如果从pid 找不到对应的processInfo
+            // processName，
+            // 则此application::onCreate 是被service 调用的，直接返回
+            return;
+        }
+        //初始化环信SDK
+        Log.d("DemoApplication", "Initialize EMChat SDK");
+        EMChat.getInstance().init(instance);
+        //获取到EMChatOptions对象
+        EMChatOptions options = EMChatManager.getInstance().getChatOptions();
+        //默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        //设置收到消息是否有新消息通知，默认为true
+        options.setNotificationEnable(false);
+        //设置收到消息是否有声音提示，默认为true
+        options.setNoticeBySound(false);
+        //设置收到消息是否震动 默认为true
+        options.setNoticedByVibrate(false);
+        //设置语音消息播放是否设置为扬声器播放 默认为true
+        options.setUseSpeaker(false);
     }
 
     private Object getRootModule() {
@@ -52,5 +88,28 @@ public class ChatApplication extends Application {
 
     public static ChatApplication getInstance() {
         return instance;
+    }
+
+    private String getAppName(int pID) {
+        String processName = null;
+        ActivityManager am = (ActivityManager) this
+                .getSystemService(ACTIVITY_SERVICE);
+        List l = am.getRunningAppProcesses();
+        Iterator i = l.iterator();
+        PackageManager pm = this.getPackageManager();
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (i
+                    .next());
+            try {
+                if (info.pid == pID) {
+                    CharSequence c = pm.getApplicationLabel(pm
+                            .getApplicationInfo(info.processName, PackageManager.GET_META_DATA));
+                    processName = info.processName;
+                    return processName;
+                }
+            } catch (Exception e) {
+            }
+        }
+        return processName;
     }
 }
