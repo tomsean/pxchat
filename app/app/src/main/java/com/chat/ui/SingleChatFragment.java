7f9e974a0435a62ce9e2fd8d98601e0923624f37
tommy <tommy.zhang@afusion.com>
2014-12-15 15:10:42 +0800

    add singlechat fragment layout

--- app/app/src/main/java/com/chat/ui/SingleChatFragment.java
+++ app/app/src/main/java/com/chat/ui/SingleChatFragment.java
@@ -1,9 +1,6 @@
 package com.chat.ui;
 
 import android.support.v4.app.FragmentActivity;
-
-/**
- * Created by HQ_19 on 2014/12/15.
- */
 public class SingleChatFragment extends FragmentActivity{
+    
 }
